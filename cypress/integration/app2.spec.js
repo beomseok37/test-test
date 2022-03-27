6099321894ef22a0635102ca0302101b32d308e7
beomseok <bmtosss@gmail.com>
2022-03-27 22:14:26 +0900

    return

--- cypress/integration/app2.spec.js
+++ cypress/integration/app2.spec.js
@@ -10,6 +10,6 @@ describe('Navigation', () => {
     cy.url().should('include', '/about');
 
     // The new page should contain an h1 with "About page"
-    cy.get('a').contains('hihihi');
+    cy.get('a').contains('hihi');
   });
 });
